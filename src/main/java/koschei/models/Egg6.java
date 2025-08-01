3793027354247e43667554dc84156180bbe88dff
SaveliyKorneev <savelvar@mail.ru>
2025-08-02 00:08:07 +0300

    2.1.4

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -6,8 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Egg6 {
 
-    @Autowired
     private Needle7 needle;
+
+    @Autowired //но можно и не указывать
+    private Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
+
 
     @Override
     public String toString() {
